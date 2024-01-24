e49b894f7f5413f6510259f902e399b7316fdc6d
Kostya-star <konstantindanilov19@hotmail.com>
2024-01-24 16:59:37 +0300

    feat: add loaders for svg jpg jpeg. make custom Button component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 
 interface NavbarProps {
   className?: string;
@@ -10,12 +11,15 @@ interface NavbarProps {
 export const Navbar: FC<NavbarProps> = ({ className }) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <AppLink to="/" theme={AppLinkTheme.SECONDARY}>
-        Main
-      </AppLink>
-      <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-        About
-      </AppLink>
+      <ThemeSwitcher/>
+      <div className={cls.links}>
+        <AppLink to="/" theme={AppLinkTheme.SECONDARY}>
+          Main
+        </AppLink>
+        <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
+          About
+        </AppLink>
+      </div>
     </div>
   );
 };
